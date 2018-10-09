3f1e472b0abad1bc2ee9b8cb3a39f36d59f8fc52
ControllerPrime <31867665+ControllerPrime@users.noreply.github.com>
2018-10-09 16:10:19 -0400

    BoundsCheck and Enemy script
    modified the BoundsCheck and Enemy Scripts

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class BoundsCheck : MonoBehaviour {
     [Header("Set in Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     // Use this for initialization
     void Awake () {
@@ -19,22 +23,40 @@ public class BoundsCheck : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight, offLeft, offUp, offDown = false;
 
         if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            isOnScreen = false;
+            offRight = true;
         }
         if (pos.x > -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            isOnScreen = false;
+            offLeft = true;
         }
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            isOnScreen = false;
+            offUp = true;
         }
         if (pos.y > -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            isOnScreen = false;
+            offDown = true;
+        }
+
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
         }
 
         transform.position = pos;
